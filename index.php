e2fd6d93bab8e44f7b53c04eb60b0bffae4dd714
petrol <konevnt@gmail.com>
2020-03-02 22:05:56 +0500

    Version 5

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ else
         <h1>Congratulations!</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
-	<p>Application version 4</p>
+	<p>Application version 5</p>
     </section>
 
     <section class="instructions">
